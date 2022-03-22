02ba919a12e04f058a34be0898b82d1c8d7fb047
LuisMoralesNoriega <ingluis07@gmail.com>
2022-03-21 20:05:31 -0600

    Arkon v 1.0
    Start test

---
+++ ArkonBackEnd/src/main/java/com/arkon/ArkonBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.arkon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkonBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkonBackEndApplication.class, args);
+	}
+
+}
